18c41de1341f2e0b4d9066917168780b8cd8636e
AbidKhairyAK <abid.khairy.ak@gmail.com>
2021-08-09 12:57:17 +0700

    tidy up menu detail

--- NewSadegMerchant/src/screens/page/main/MenuDetail/index.js
+++ NewSadegMerchant/src/screens/page/main/MenuDetail/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, View, Image, TouchableOpacity } from 'react-native'
+import { ScrollView, View, Image, TouchableOpacity, StyleSheet } from 'react-native'
 
 import { ShadowView, BaseText, BaseIcon, MoneyText, BaseSwitch, BaseButton } from '@/components'
 import { sizes, colors } from '@/constants'
@@ -9,43 +9,43 @@ const MenuDetail = ({ navigation, route }) => {
 	const { menu } = route.params
 
 	return (
-		<ScrollView style={{ position: 'relative' }}>
+		<ScrollView style={styles.container}>
 			<BaseButton 
 				onPress={navigation.goBack}
 				icon="chevron-back-outline" 
 				radius="xxs"
-				style={{ position: 'absolute', top: sizes.base, left: sizes.base, zIndex: 1 }} 
-				innerStyle={{ paddingVertical: 0, width: sizes.base * 2, height: sizes.base * 2 }}
+				style={styles.backButton} 
+				innerStyle={styles.backButtonInner}
 			/>
 
 			<Image
 				source={{ uri: menu.image }}
-				style={{ width: '100%', height: getScreenSize().width }}
+				style={styles.image}
 			/>
 
-			<ShadowView type="sectionHard" style={{ width: '100%', borderRadius: sizes.base, marginTop: sizes.base * -2 }}>
-				<View style={{ minHeight: getScreenSize().height / 2 + sizes.base * 2, width: '100%', backgroundColor: colors.white, padding: sizes.xl, borderTopLeftRadius: sizes.base, borderTopRightRadius: sizes.base, position: 'relative' }}>
-					<BaseText type="semi-bold" size="xl" style={{ marginBottom: sizes.xxxs }}>
+			<ShadowView type="sectionHard" style={styles.contentShadow}>
+				<View style={styles.contentWrapper}>
+					<BaseText type="semi-bold" size="xl" style={styles.title}>
 						{toTitleCase(menu.name)}
 					</BaseText>
 
-					<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: sizes.xl }}>
+					<View style={styles.priceWrapper}>
 						<MoneyText value={menu.price} size="xxl"/>
 						<BaseSwitch
 							status={menu.status === 'ready'}
 							trueTitle="IN" 
 							falseTitle="OUT" 
-							style={{ marginTop: sizes.base / 4 }} 
+							style={styles.status} 
 						/>
 					</View>
 
-					<BaseText style={{ marginBottom: (sizes.xl * 2) + (sizes.xs * 2) + sizes.lg }}>
+					<BaseText style={styles.description}>
 						{menu.description}		
 					</BaseText>
 
-					<View style={{ flexDirection: 'row', justifyContent: 'space-between', position: 'absolute', bottom: sizes.xl, left: sizes.xl, right: sizes.xl }}>
-						<BaseButton title="Delete Menu" icon="trash-outline" bg="red" color="white" style={{ width: '46%' }} />
-						<BaseButton title="Edit Menu" icon="create-outline" bg="green" color="white" style={{ width: '46%' }} />
+					<View style={styles.actionWrapper}>
+						<BaseButton title="Delete Menu" icon="trash-outline" bg="red" color="white" style={styles.actionButton} />
+						<BaseButton title="Edit Menu" icon="create-outline" bg="green" color="white" style={styles.actionButton} />
 					</View>
 				</View>
 			</ShadowView>
@@ -54,3 +54,18 @@ const MenuDetail = ({ navigation, route }) => {
 }
 
 export default MenuDetail
+
+const styles = StyleSheet.create({
+	container: { position: 'relative' },
+	backButton: { position: 'absolute', top: sizes.base, left: sizes.base, zIndex: 1 },
+	backButtonInner: { paddingVertical: 0, width: sizes.base * 2, height: sizes.base * 2 },
+	image: { width: '100%', height: getScreenSize().width },
+	contentShadow: { width: '100%', borderRadius: sizes.base, marginTop: sizes.base * -2 },
+	contentWrapper: { minHeight: getScreenSize().height / 2 + sizes.base * 2, width: '100%', backgroundColor: colors.white, padding: sizes.xl, borderTopLeftRadius: sizes.base, borderTopRightRadius: sizes.base, position: 'relative' },
+	title: { marginBottom: sizes.xxxs },
+	priceWrapper: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: sizes.xl },
+	status: { marginTop: sizes.base / 4 },
+	description: { marginBottom: (sizes.xl * 2) + (sizes.xs * 2) + sizes.lg },
+	actionWrapper: { flexDirection: 'row', justifyContent: 'space-between', position: 'absolute', bottom: sizes.xl, left: sizes.xl, right: sizes.xl },
+	actionButton: { width: '46%' }
+})
