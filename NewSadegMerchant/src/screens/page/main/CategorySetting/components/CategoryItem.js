fbdcf9f08c96d477247b1582641710c43ca27545
AbidKhairyAK <abid.khairy.ak@gmail.com>
2021-08-09 17:52:15 +0700

    add category setting screen

--- NewSadegMerchant/src/screens/page/main/CategorySetting/components/CategoryItem.js
+++ NewSadegMerchant/src/screens/page/main/CategorySetting/components/CategoryItem.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, TextInput, StyleSheet } from 'react-native'
 
 import { BaseText, BaseButton, BaseIcon } from '@/components'
 import { sizes, colors, fontTypes } from '@/constants'
 
 const CategoryItem = ({ cat }) => {
+	const [isFocus, setIsFocus] = useState(false)
+
+	const changeFocus = val => e => setIsFocus(val)
+
 	return (
 		<View style={styles.container}>
 			<TextInput
-				style={styles.input}
+				style={styles.input(isFocus)}
 				value={cat}
 				placeholder="Category name ..."
+				onFocus={changeFocus(true)}
+				onEndEditing={changeFocus(false)}
 			/>
 
 			<View style={styles.itemWrapper}>
@@ -29,7 +35,17 @@ export default CategoryItem
 
 const styles = StyleSheet.create({
 	container: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
-	input: { padding: 0, paddingVertical: sizes.xxxs, paddingLeft: sizes.xxxs, marginLeft: sizes.xxxs * -1, fontFamily: fontTypes.regular, fontSize: sizes.sm, borderWidth: 0, borderRadius: sizes.xs, borderColor: colors.green, width: '40%' },
+	input: isFocus => ({ 
+		backgroundColor: colors[isFocus ? 'lightGray' : 'white'],
+		padding: 0,
+		paddingVertical: sizes.xxxs,
+		paddingLeft: sizes.xxxs,
+		marginLeft: sizes.xxxs * -1,
+		fontFamily: fontTypes.regular,
+		fontSize: sizes.sm,
+		borderRadius: sizes.xs,
+		width: '40%' 
+	}),
 	itemWrapper: { flexDirection: 'row', alignItems: 'center' },
 	itemIcon: { marginRight: sizes.base / 4 },
 	buttonInner: { paddingVertical: sizes.base / 3, paddingHorizontal: sizes.base / 3 }
